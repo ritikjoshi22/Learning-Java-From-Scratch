e7c2a10cf5786bd93f3b1e289e931a3ca522c522
ritikjoshi22 <joshster.ritik@gmail.com>
2024-11-26 13:14:56 +0545

    implementation of scanner and javax(gui) for user input

---
+++ src/practice/Main.java
@@ -0,0 +1,13 @@
+package practice;
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+
+	public static void main(String[] args) {
+//		user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello! " + name);
+	}
+}
