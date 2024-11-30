3c24858a6651952a04502137731515c8dcb937e7
ritikjoshi22 <joshster.ritik@gmail.com>
2024-11-30 12:15:36 +0545

    Jframe implementation for gui

---
+++ src/jFrame/Main.java
@@ -0,0 +1,25 @@
+package jFrame;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		JFrame frame = new JFrame(); //creates a frame
+//		frame.setTitle("Hello Ritik"); // sets the tile of the frame
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+//		frame.setSize(420,420); // set the size of the frame
+//		frame.setResizable(false);// prevent frame from being resize
+//		frame.setVisible(true); // make the frame visible on the screen
+//		
+//		ImageIcon image =  new ImageIcon("logo.png"); // creates an image icon
+//		frame.setIconImage(image.getImage()); // set icon of the frame
+//		frame.getContentPane().setBackground(new Color(0,255,0)); //change background color of the frame
+	
+	MyFrame myFrame = new MyFrame();
+	}
+}
---
+++ src/jFrame/MyFrame.java
@@ -0,0 +1,22 @@
+package jFrame;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	MyFrame(){
+		
+		this.setTitle("Hello Ritik"); // sets the tile of the this
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+		this.setSize(420,420); // set the size of the this
+		this.setResizable(false);// prevent this from being resize
+		this.setVisible(true); // make the this visible on the screen
+		
+		ImageIcon image =  new ImageIcon("logo.png"); // creates an image icon
+		this.setIconImage(image.getImage()); // set icon of the this
+		this.getContentPane().setBackground(new Color(0,255,0)); //change background color of the this
+	}
+}
