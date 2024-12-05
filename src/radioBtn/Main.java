ff03efe1df1e6ae7d557d6ab494d9e5dd162bf2f
ritikjoshi22 <joshster.ritik@gmail.com>
2024-12-05 13:57:11 +0545

    radio btn implementation

---
+++ src/radioBtn/Main.java
@@ -0,0 +1,11 @@
+package radioBtn;
+public class Main{
+
+	 public static void main(String[] args) {
+	 
+	  // JRadioButton = One or more buttons in a grouping in which only 1 may be selected per group
+	  
+	  new MyFrame();
+
+	 }
+	}
