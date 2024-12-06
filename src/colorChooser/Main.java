ca1030f0fe93e93eb5c802e763e7567c26bc7b72
ritikjoshi22 <joshster.ritik@gmail.com>
2024-12-06 15:01:07 +0545

    JColorChooser implementation

---
+++ src/colorChooser/Main.java
@@ -0,0 +1,10 @@
+package colorChooser;
+public class Main{
+
+	public static void main(String[] args) {
+	
+		// JColorChooser = A GUI mechanism that let's a user choose a color
+		
+		new MyFrame();
+	}
+}
