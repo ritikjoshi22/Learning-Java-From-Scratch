2da3a93d695e523a96729c131781790675d3e459
ritikjoshi22 <joshster.ritik@gmail.com>
2024-12-07 11:56:56 +0545

    implementation of generic classes and methods

---
+++ src/genericClass/MyGenericClass.java
@@ -0,0 +1,16 @@
+package genericClass;
+public class MyGenericClass <Thing extends Number, Thing2 extends Number>{
+
+	Thing x;
+	Thing2 y;
+	
+	MyGenericClass(Thing x, Thing2 y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Thing getValue() {
+		return x;
+		
+	}
+}
