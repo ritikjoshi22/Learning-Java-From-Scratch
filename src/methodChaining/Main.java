6189f864874c2509e287966f54c4e9044b4d2e53
ritikjoshi22 <joshster.ritik@gmail.com>
2024-12-08 23:15:06 +0545

    method chaining implementation

--- src/methodChaining/Main.java
+++ src/methodChaining/Main.java
@@ -1,5 +1,20 @@
 package methodChaining;
-
 public class Main {
 
+	public static void main(String[] args) {
+		
+		// method chaining = 	a common syntax for invoking multiple method calls in OOP
+		//						condense code into less lines
+		
+		String name = "      bro";
+		
+		//name = name.concat(" code   ");
+		//name = name.toUpperCase();
+		//name = name.trim();
+		
+		name = name.concat("code").toUpperCase().trim();
+		
+		System.out.println(name);
+			
+	}
 }
